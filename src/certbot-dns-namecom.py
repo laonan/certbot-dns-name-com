5567bae0fa515e9176e9fee3a52327396829e84f
Kael Shipman <kael.shipman@gmail.com>
2021-12-15 19:12:16 -0600

    Bugfixes

--- src/certbot-dns-namecom.py
+++ src/certbot-dns-namecom.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import time
 import os
 import sys
 import json
@@ -22,9 +23,9 @@ class NameComDNS:
         url = 'https://api.name.com/v4/domains/%s/records' % self.domain_name
         r = requests.post(url, data=json.dumps(data), auth=(self.username, self.token))
         if r.status_code == 200 or r.status_code == 201:
-            print(r.json())
+            return r.json()
         else:
-            print('%s: %s' % (r.status_code, r.content))
+            raise Exception('name.com API %s Error: %s' % (r.status_code, r.content))
 
     def del_record(self, record_id):
         url = 'https://api.name.com/v4/domains/%s/records/%s' % (self.domain_name, record_id)
@@ -36,7 +37,9 @@ class NameComDNS:
 if __name__ == '__main__':
 
     # Get command line arguments
-    file_name, cmd = sys.argv
+    cmd = sys.argv[0]
+    if (cmd != "clean"):
+        cmd = "add"
 
     # Get and validate environment variables
     certbot_domain = os.environ.get('CERTBOT_DOMAIN')
@@ -66,6 +69,7 @@ if __name__ == '__main__':
 
     username = config['namecom']['username']
     token = config['namecom']['token']
+    waitsec = config.get('waitsec', 30)
 
     if (username is None):
         raise Exception("Missing field 'namecom.username' in config file")
@@ -73,19 +77,25 @@ if __name__ == '__main__':
         raise Exception("Missing field 'namecom.token' in config file")
 
     # Instantiate our class
-    ncd = NameComDNS(certbot_domain, username, token)
+    ncd = NameComDNS(username, token, certbot_domain)
 
     if cmd == 'add':
         # If we're adding a record, create it
+        print("Adding records to name.com DNS")
         ncd.create_record(data)
 
     elif cmd == 'clean':
         # Otherwise, delete
+        print("Deleting ACME records from name.com DNS")
         j = ncd.list_records()
 
         for record in j['records']:
             if record['host'] == '_acme-challenge':
                 ncd.del_record(record['id'])
+
+    if (waitsec != 0):
+        print("Waiting %s seconds for DNS to propagate" % waitsec)
+        time.sleep(waitsec)
 
 
 
